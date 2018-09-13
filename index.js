b6db8acb963447de82a07edd194042ccda056d99
Christiaan Mealey <chris.mealey@gmail.com>
2018-09-13 09:00:48 -0400

    wip

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express   = require('express'),
-      app       = express(),
-      http      = require('http'),
-      gSheets   = require('./lib/sheets');
+const express     = require('express'),
+      app         = express(),
+      cors        = require('cors'),
+      bodyParser  = require('body-parser'),
+      http        = require('http'),
+      gSheets     = require('./lib/sheets');
 
 app.set('port', process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function(req, res, next){
   res.status(200).send('ok');
   next();
 });
 
-app.get('/update', function(req, res, next) {
-  gSheets.loadAuth(gSheets.onAuthorized);
-  res.status(200).send('ok');
+app.post('/update', function(req, res, next) {
+  let payload = req.query;
+  let values = [];
+  for(prop in payload) {
+    values.push(payload[prop]);
+  }
+  gSheets.loadAuth(gSheets.onAuthorized, values);
+  res.status(200).send(payload);
   next();
 });
 
