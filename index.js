3ced52b579e021e0eafdd8fdb3091f22124a240d
Christiaan Mealey <chris.mealey@gmail.com>
2018-09-21 15:58:43 -0400

    sourcing tool and server

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express     = require('express'),
       cors        = require('cors'),
       bodyParser  = require('body-parser'),
       http        = require('http'),
-      gSheets     = require('./lib/sheets');
+      gSheets     = require('./lib/sheets'),
+      MongoClient = require('mongodb').MongoClient,
+      apicache    = require('apicache');
+
+// Connection url
+const url = 'mongodb://carvio:__CArv10__@18.188.101.17:27017/carvio';
+// Database Name
+const dbName = 'carvio';
+const cache = apicache.options({debug: true}).middleware;
 
 app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.json());
@@ -27,4 +35,45 @@ app.post('/update', function(req, res, next) {
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('app listening on port ' + app.get('port'));
+});
+
+app.get('/api/v1/makes', cache('3600 minutes'), function(req, res, next) {
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+    if(err) console.error(err);
+    const makes = client.db(dbName).collection('makes');
+    makes.find({}).toArray(function(err, items) {
+      res.json(items);
+      client.close();
+    });
+  });
+});
+
+app.get('/api/v1/models/:make', cache('3600 minutes'), function(req, res, next) {
+  const params = req.params;
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+    if(err) console.error(err);
+    const models = client.db(dbName).collection('models');
+    const make = params.make;
+    models.find({'make': make}).toArray(function(err, items) {
+      res.json(items);
+      client.close();
+    });
+  });
+});
+
+app.get('/api/v1/model/:model', cache('3600 minutes'), function(req, res, next) {
+  const params = req.params;
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+    if(err) console.error(err);
+    const models = client.db(dbName).collection('models');
+    const model = params.model;
+    models.find({'model': model}).toArray(function(err, items) {
+      res.json(items);
+      client.close();
+    });
+  });
+});
+
+app.get('/api/cache/index', function(req, res, next) {
+  res.json(apicache.getIndex());
 });
